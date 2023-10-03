63a7675f7270598deb437912d2e5da528fc39deb
commodore <harryrockfellaz@gmail.com>
2023-10-03 18:00:26 +0100

    First Version

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Full stack blog</title>
+
+    </head>
+    <body class="antialiased">
+       <h1>This is the body</h1>
+    </body>
+</html>
